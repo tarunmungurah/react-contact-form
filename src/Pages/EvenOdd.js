6cd8c95245186af287c62165e66e11945f788123
Rak <hfr@esokia.com>
2022-11-11 13:20:48 +0400

    review 2

--- src/Pages/EvenOdd.js
+++ src/Pages/EvenOdd.js
@@ -1,46 +1,54 @@
-import React, { useState } from "react";
+import React, { useState } from 'react';
 
-function EvenOdd(){
+function EvenOdd() {
   const [number, setNumber] = useState('');
   const [isSubmitted, setisSubmitted] = useState(false);
 
-  const numValidation=()=>{
-    if(number % 2 === 0) {
-        alert("The number is even.");
-    }   
+  const numValidation = () => {
+    if (number % 2 === 0) {
+      alert('The number is even.');
+    }
     // if the number is odd
     else {
-        alert("The number is odd.");
+      alert('The number is odd.');
     }
-};
+  };
 
   function OnChangeNumber(text) {
     console.log(text.target.value);
     setNumber(text.target.value);
   }
 
-  function OnSubmitForm() {
-    numValidation()
+  function OnSubmitForm(e) {
+    numValidation();
     setisSubmitted(true);
-    
+    e.preventDefault(); // EMPECHE L'EVENEMET A PROPAGER , C POUR CELA QU'AVANT IL Y A LE REFRESH
   }
-    return (
-        <div className="container mt-5">
-      <form onSubmit>
+  return (
+    <div className="container mt-5">
+      <form>
+        {' '}
+        {/** enleve le on submit  car tu gere deja l'action avec le ONCLCIK*/}
         <div className="mb-3">
           <label className="form-label" htmlFor="number">
-           Input number
+            Input number
           </label>
-          <input className="form-control" type="text" id="number" onChange={OnChangeNumber} value={number} required />
+          <input
+            className="form-control"
+            type="text"
+            id="number"
+            onChange={OnChangeNumber}
+            value={number}
+            required
+          />
           {isSubmitted && number === '' && <p> This field is required </p>}
         </div>
         <button className="btn btn-danger" onClick={OnSubmitForm}>
           SUBMIT
         </button>
-        </form>
+      </form>
     </div>
-    )
+  );
 }
-
 
 export default EvenOdd;
