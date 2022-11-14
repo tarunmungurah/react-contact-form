3d8388fd09b82641fc2791e56d43a19baffa1707
tarun <tm@esokia-webagency.com>
2022-11-14 15:58:47 +0400

    EvenOdd/NumberValidationCorrection

--- src/Pages/EvenOdd.js
+++ src/Pages/EvenOdd.js
@@ -1,33 +1,36 @@
-import React, {
-  useState
-} from 'react';
+import React, { useState } from 'react';
 
 function EvenOdd() {
   const [number, setNumber] = useState('');
   const [isSubmitted, setisSubmitted] = useState(false);
 
-  const [message, setMessage] = useState('');
-
+  const[message, setMessage] = useState('');
 
   const numValidation = () => {
-    const regEx = /^-?\d+$/ //to check positive and negative number
+    const regEx = /^-?\d{1,3}(,\d{3})*$/
     if (regEx.test(number) && number % 2 === 0) {
       setMessage('The number is even.');
-    } else if (regEx.test(number) && number % 2 !== 0) {
+    }
+    else if (regEx.test(number) && number % 2 !== 0) {
       setMessage('The number is odd.');
-    } else if (number === "") {
+    }
+    else if (number ===  "" ) {
       setMessage('');
-    } else {
-      setMessage('Number format not good');
+    }
+    else {
+      setMessage('Number format not good!!');
     }
   };
-
-
 
   function OnChangeNumber(text) {
     console.log(text.target.value);
     setNumber(text.target.value);
     setMessage('');
+   
+    if(isNaN(text.target.value)){
+      console.log(text.target.value + " is not a number <br/>");
+      setMessage('Please enter a number');
+     }
   }
 
   function OnSubmitForm(e) {
@@ -35,44 +38,31 @@ function EvenOdd() {
     setisSubmitted(true);
     e.preventDefault(); // EMPECHE L'EVENEMET A PROPAGER , C POUR CELA QU'AVANT IL Y A LE REFRESH
   }
-  return ( <
-    div className = "container mt-5" >
-    <
-    form > {
-      ' '
-    } {
-      /** enleve le on submit  car tu gere deja l'action avec le ONCLCIK*/
-    } <
-    div className = "mb-3" >
-    <
-    label className = "form-label"
-    htmlFor = "number" >
-    Input number <
-    /label> <
-    input className = "form-control"
-    type = "text"
-    id = "number"
-    onChange = {
-      OnChangeNumber
-    }
-    value = {
-      number
-    }
-    required /
-    >
-    {
-      isSubmitted && number === '' && < p > This field is required < /p>} {
-      message
-    } <
-    /div> <
-    button className = "btn btn-danger"
-    onClick = {
-      OnSubmitForm
-    } >
-    SUBMIT <
-    /button> < /
-    form > <
-    /div>
+  return (
+    <div className="container mt-5">
+      <form>
+        {' '}
+        {/** enleve le on submit  car tu gere deja l'action avec le ONCLCIK*/}
+        <div className="mb-3">
+          <label className="form-label" htmlFor="number">
+            Input number
+          </label>
+          <input
+            className="form-control"
+            type="text"
+            id="number"
+            onChange={OnChangeNumber}
+            value={number}
+            required
+          />
+          {isSubmitted && number === '' && <p> This field is required </p>}
+          {message}
+        </div>
+        <button className="btn btn-danger" onClick={OnSubmitForm}>
+          SUBMIT
+        </button>
+      </form>
+    </div>
   );
 }
 
